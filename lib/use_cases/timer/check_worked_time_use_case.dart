8517bb68101b8afd4fdce0e4b33b763d600e20ca
Patryk Połeć <patryk.polec@snow.dog>
2020-06-14 12:35:22 +0200

    fixed check worked time use case logic

--- lib/use_cases/timer/check_worked_time_use_case.dart
+++ lib/use_cases/timer/check_worked_time_use_case.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CheckWorkedTimeUseCase {
-  String checkWorkedHours({@required int value}) =>
+  String checkWorkedTime({@required int value}) =>
+      '${_checkWorkedHours(value: value)}:${_checkWorkedMinutes(value: value)}:${_checkWorkedSeconds(value: value)}';
+
+  String _checkWorkedHours({@required int value}) =>
       (value / 3600).floor().toString().padLeft(2, '0');
 
-  String checkWorkedMinutes({@required int value}) =>
+  String _checkWorkedMinutes({@required int value}) =>
       ((value / 60) % 60).floor().toString().padLeft(2, '0');
 
-  String checkWorkedSeconds({@required int value}) =>
+  String _checkWorkedSeconds({@required int value}) =>
       (value % 60).floor().toString().padLeft(2, '0');
 }
